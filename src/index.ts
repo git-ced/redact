89ba5425542e6e3186b3137fb189a5b27dddb71d
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-26 03:14:03 +0800

    Initialize initial functions

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,28 @@
-export const sample = () => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('sample');
-  }
+/**
+ * Returns a distinct values for all the blacklisted words given
+ *
+ * @param {Array<string>} args - Word to be blacklisted.
+ */
+export const blacklist = () => { };
 
-  return 'sample';
-};
+/**
+ * Evaluate a string if its in the blacklist and
+ * return an edited version.
+ *
+ * @param {string} string - Sentence to filter.
+ */
+export const redact = () => { };
+
+/**
+ * Returns if the sentence has a blacklisted word
+ *
+ * @param {string} string - Sentence to lookup.
+ */
+export const isBlacklisted = () => { };
+
+/**
+ * Returns the count of blacklisted words in the sentence
+ *
+ * @param {string} string - Sentence to lookup.
+ */
+export const blacklistCount = () => { };
