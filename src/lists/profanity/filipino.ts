8a0747a35d8d9709b8ff33192e86aa29c106559f
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-25 23:21:18 +0800

    Create filipino profanity list

---
+++ src/lists/profanity/filipino.ts
@@ -0,0 +1,127 @@
+export const filipino: string[] = [
+  // SECTION A
+  "amputa",
+  "animal ka",
+  // SECTION B
+  "bilat",
+  "binibrocha",
+  "bobo",
+  "bogo",
+  "boto",
+  "brocha",
+  "burat",
+  "bwesit",
+  "bwisit",
+  "bwiset",
+  // SECTION C
+  // SECTION D
+  "dimonyo",
+  "demonyo",
+  // SECTION E
+  "engot",
+  "etits",
+  // SECTION F
+  // SECTION G
+  "gaga",
+  "gagi",
+  "gago",
+  // SECTION H
+  "habal",
+  "hayop ka",
+  "hayup",
+  "hinampak",
+  "hinayupak",
+  "hindot",
+  "hindutan",
+  "hudas",
+  // SECTION I
+  "iniyot",
+  "inutel",
+  "inutil",
+  "iyot",
+  // SECTION J
+  // SECTION K
+  "kagaguhan",
+  "kagang",
+  "kantot",
+  "kantotan",
+  "kantut",
+  "kantutan",
+  "kaululan",
+  "kayat",
+  "kiki",
+  "kikinginamo",
+  "kingina",
+  "kupal",
+  // SECTION L
+  "leche",
+  "leching",
+  "lechugas",
+  "lintik",
+  // SECTION M
+  // SECTION N
+  "nakakaburat",
+  "nimal",
+  // SECTION O
+  "ogag",
+  "olok",
+  // SECTION P
+  "pakingshet",
+  "pakshet",
+  "pakyu",
+  "pesteng yawa",
+  "poke",
+  "poki",
+  "pokpok",
+  "poyet",
+  "pu'keng",
+  "pucha",
+  "puchanggala",
+  "puchangina",
+  "puke",
+  "puki",
+  "pukinangina",
+  "puking",
+  "punyeta",
+  "puta",
+  "putang",
+  "putang ina",
+  "putangina",
+  "putanginamo",
+  "putaragis",
+  "putragis",
+  "puyet",
+  // SECTION Q
+  // SECTION R
+  "ratbu",
+  // SECTION S
+  "shunga",
+  "sira ulo",
+  "siraulo",
+  "suso",
+  "susu",
+  // SECTION T
+  "tae",
+  "taena",
+  "tamod",
+  "tanga",
+  "tangina",
+  "taragis",
+  "tarantado",
+  "tete",
+  "teti",
+  "timang",
+  "tinil",
+  "tite",
+  "titi",
+  "tungaw",
+  // SECTION U
+  "ulol",
+  "ulul",
+  "ungas",
+  // SECTION V
+  // SECTION W
+  // SECTION X
+  // SECTION Y
+  // SECTION Z
+];
