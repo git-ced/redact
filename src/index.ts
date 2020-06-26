1b8b1b8598a56f8b2483665afff66fae6750a9a7
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-06-26 09:55:44 +0800

    Add whitelist and backlist

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,30 @@
+const uniqueOnly = (arr: string[]) => (
+  arr.filter((v, i, a) => a.indexOf(v) === i)
+)
+
 /**
  * Returns a distinct values for all the blacklisted words given
  *
- * @param {Array<string>} args - Word to be blacklisted.
+ * @param {Array<string>} args Word to be blacklisted.
  */
-export const blacklist = () => { };
+export const blacklist = (...args: string[]) => uniqueOnly(args);
+
+/**
+ * Filters the blacklist to allow some given words
+ *
+ * @param {Array<string>} blacklist Blacklisted words.
+ * @param {Array<string>} args Word to be whitelisted.
+ */
+export const whitelist = (blacklist: string[], ...args: string[]) => uniqueOnly(blacklist.filter((b) => !args.includes(b)));
 
 /**
  * Evaluate a string if its in the blacklist and
  * return an edited version.
  *
  * @param {string} string - Sentence to filter.
+ * @param {Array<string>} blacklist Blacklisted words.
  */
-export const redact = () => { };
+export const redact = (string: string, blacklist: string[]) => { };
 
 /**
  * Returns if the sentence has a blacklisted word
