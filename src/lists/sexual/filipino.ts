e77a7ed5bc98d4fe85afd286499cd152b923751c
Prince Neil Cedrick Castro <cedi.castro@gmail.com>
2020-07-20 10:47:19 +0800

    Add sexually explicity filipino words

---
+++ src/lists/sexual/filipino.ts
@@ -0,0 +1,63 @@
+export const filipino: string[] = [
+  // SECTION A
+  // SECTION B
+  "bilat",
+  "boto",
+  "brocha",
+  "burat",
+  // SECTION C
+  // SECTION D
+  // SECTION E
+  "etits",
+  // SECTION F
+  // SECTION G
+  // SECTION H
+  "hindot",
+  // SECTION I
+  "iniyot",
+  "iyot",
+  // SECTION J
+  // SECTION K
+  "kantot",
+  "kantotan",
+  "kantut",
+  "kantutan",
+  "kaululan",
+  "kiki",
+  // SECTION L
+  // SECTION M
+  // SECTION N
+  "nakakaburat",
+  // SECTION O
+  // SECTION P
+  "poke",
+  "poki",
+  "poyet",
+  "pu'keng",
+  "puke",
+  "puki",
+  "puking",
+  "putaragis",
+  "putragis",
+  "puyet",
+  "puwet",
+  "pwet",
+  // SECTION Q
+  // SECTION R
+  "ratbu",
+  // SECTION S
+  "suso",
+  "susu",
+  // SECTION T
+  "tamod",
+  "tete",
+  "teti",
+  "tite",
+  "titi",
+  // SECTION U
+  // SECTION V
+  // SECTION W
+  // SECTION X
+  // SECTION Y
+  // SECTION Z
+];
